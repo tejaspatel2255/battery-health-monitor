8a20357a6310f2c0cc2866efb1cca9e56b79d77d
tejaspatel2255 <tejaspatel22505@gmail.com>
2026-08-02 02:10:14 +0530

    Enable Android core library desugaring for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -59,6 +60,10 @@ android {
             )
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
